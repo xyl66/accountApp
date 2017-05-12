97281599a3c511a385771c124e28fcb7eb6167c3
xyl <492113976@qq.com>
2017-05-12 16:45:59 +0800

    first commit

---
+++ js/type-options.js
@@ -0,0 +1,454 @@
+const typeOptions=[{
+				        value: '110000',
+				        text: '食品酒水',
+				        children: [{
+				                value: "110101",
+				                text: "早午晚餐"
+				        },
+				        {
+				                value: "110102",
+				                text: "水果零食"
+				        },
+				        {
+				                value: "110103",
+				                text: "粮油米面"
+				        },{
+				        	value:"110104",
+				        	text:"酒水饮料"
+				        },{
+				        	value:"110105",
+				        	text:"外出美食"
+				        }
+				        ]
+				    }, {
+				        value: '120000',
+				        text: '购物消费',
+				        children: [{
+					        value: "120101",
+				            text: "衣裤鞋帽"
+				        }, {
+				            value: "120102",
+				            text: "洗护用品"
+				        }, {
+				            value: "120104",
+				            text: "厨房用品"
+				        }, {
+				            value: "120105",
+				            text: "家用纺织"
+				        }, {
+				            value: "120106",
+				            text: "清洁用品"
+				        }, {
+				            value: "120107",
+				            text: "家居日用"
+				        }, {
+				            value: "120108",
+				            text: "家居饰品"
+				        }, {
+				            value: "120109",
+				            text: "家具家电"
+				        }, {
+				            value: "120110",
+				            text: "汽车用品"
+				        }, {
+				            value: "120111",
+				            text: "电子数码"
+				        }, {
+				            value: "120112",
+				            text: "美妆护肤"
+				        }, {
+				            value: "120113",
+				            text: "办公用品"
+				        }, {
+				            value: "120114",
+				            text: "书报杂志"
+				        }, {
+				            value: "120115",
+				            text: "运动器械"
+				        }, {
+				            value: "120116",
+				            text: "珠宝首饰"
+				        }
+				        ]
+				    },{
+				    	value:'130000',
+				    	text:'居家生活',
+				    	children:[{
+				    		value:"130101",
+				    		text:"房租物业"
+				    	},
+				    	{
+				    		value:"130102",
+				    		text:"宽带话费"
+				    	},
+				    	{
+				    		value:"130103",
+				    		text:"水电燃气"
+				    	},
+				    	{
+				    		value:"130104",
+				    		text:"维修人工"
+				    	}
+				    	]
+				    },{
+				    	value:'140000',
+				    	text:'行车交通',
+				    	children:[{
+				    		value:"140101",
+				    		text:"加油"
+				    	},
+				    	{
+				    		value:"140102",
+				    		text:"停车"
+				    	},
+				    	{
+				    		value:"140103",
+				    		text:"保养"
+				    	},
+				    	{
+				    		value:"140104",
+				    		text:"维修"
+				    	},
+				    	{
+				    		value:"140105",
+				    		text:"保险"
+				    	},
+				    	{
+				    		value:"140106",
+				    		text:"违章罚款"
+				    	},
+				    	{
+				    		value:"140107",
+				    		text:"公共交通"
+				    	},
+				    	{
+				    		value:"140108",
+				    		text:"打车租车"
+				    	},
+				    	{
+				    		value:"140109",
+				    		text:"飞机火车"
+				    	},
+				    	{
+				    		value:"140110",
+				    		text:"驾照考试"
+				    	},
+				    	{
+				    		value:"140111",
+				    		text:"共享单车"
+				    	}
+				    	]
+				    },{
+				    	value:'150000',
+				    	text:'休闲娱乐',
+				    	children:[{
+				    		value:"150101",
+				    		text:"电影"
+				    	},
+				    	{
+				    		value:"150102",
+				    		text:"K歌"
+				    	},
+				    	{
+				    		value:"150103",
+				    		text:"运动"
+				    	},
+				    	{
+				    		value:"150104",
+				    		text:"网游"
+				    	},
+				    	{
+				    		value:"150105",
+				    		text:"话剧"
+				    	},
+				    	{
+				    		value:"150106",
+				    		text:"麻将"
+				    	},
+				    	{
+				    		value:"150107",
+				    		text:"棋牌"
+				    	},
+				    	{
+				    		value:"150108",
+				    		text:"聚会"
+				    	},
+				    	{
+				    		value:"150109",
+				    		text:"宠物"
+				    	},
+				    	{
+				    		value:"150110",
+				    		text:"洗浴"
+				    	},
+				    	{
+				    		value:"150111",
+				    		text:"温泉"
+				    	},
+				    	{
+				    		value:"150104",
+				    		text:"演唱会"
+				    	},
+				    	{
+				    		value:"150104",
+				    		text:"其它"
+				    	}
+				    	]
+				    },{
+				    	value:'160000',
+				    	text:'出差旅游',
+				    	children:[{
+				    		value:"160101",
+				    		text:"交通费"
+				    	},
+				    	{
+				    		value:"160102",
+				    		text:"住宿费"
+				    	},
+				    	{
+				    		value:"160103",
+				    		text:"娱乐费"
+				    	},
+				    	{
+				    		value:"160104",
+				    		text:"餐饮费"
+				    	},
+				    	{
+				    		value:"160105",
+				    		text:"出行用品"
+				    	},
+				    	{
+				    		value:"160106",
+				    		text:"其他消费"
+				    	}
+				    	]
+				    },{
+				    	value:'170000',
+				    	text:'学习进修',
+				    	children:[{
+				    		value:"170101",
+				    		text:"书报杂志"
+				    	},
+				    	{
+				    		value:"170102",
+				    		text:"培训进修"
+				    	},
+				    	{
+				    		value:"170103",
+				    		text:"数码装备"
+				    	}
+				    	]
+				    },{
+				    	value:'180000',
+				    	text:'金融保险',
+				    	children:[{
+				    		value:"180101",
+				    		text:"房屋首付"
+				    	},
+				    	{
+				    		value:"180102",
+				    		text:"汽车首付"
+				    	},
+				    	{
+				    		value:"180103",
+				    		text:"房贷车贷"
+				    	},
+				    	{
+				    		value:"180104",
+				    		text:"税费杂费"
+				    	},
+				    	{
+				    		value:"180103",
+				    		text:"人身保险"
+				    	}
+				    	]
+				    },{
+				    	value:'190000',
+				    	text:'医疗保健',
+				    	children:[{
+				    		value:"190101",
+				    		text:"日常药品"
+				    	},
+				    	{
+				    		value:"190102",
+				    		text:"保健药品"
+				    	},
+				    	{
+				    		value:"190103",
+				    		text:"门诊住院"
+				    	},
+				    	{
+				    		value:"190104",
+				    		text:"中医推拿"
+				    	}
+				    	]
+				    },{
+				    	value:'200000',
+				    	text:'装修设计',
+				    	children:[{
+				    		value:"200101",
+				    		text:"装修材料"
+				    	},
+				    	{
+				    		value:"200102",
+				    		text:"家电家具"
+				    	},
+				    	{
+				    		value:"200103",
+				    		text:"装修装饰"
+				    	},
+				    	{
+				    		value:"200104",
+				    		text:"装修人工"
+				    	},
+				    	{
+				    		value:"200103",
+				    		text:"装修其它"
+				    	}
+				    	]
+				    },{
+				    	value:'210000',
+				    	text:'其他杂项',
+				    	children:[{
+				    		value:"210101",
+				    		text:"意外丢失"
+				    	},
+				    	{
+				    		value:"210102",
+				    		text:"其他支出"
+				    	}
+				    	]
+				    }
+				    ];
+const accountOptions=[{
+				        value: '110000',
+				        text: '现金账户',
+				        children: [{
+				                value: "110101",
+				                text: "家庭公用"
+				        },
+				        {
+				                value: "110102",
+				                text: "老公钱包"
+				        },
+				        {
+				                value: "110103",
+				                text: "媳妇钱包"
+				        }
+				        ]
+				    }, {
+				        value: '120000',
+				        text: '信用卡',
+				        children: [{
+					        value: "120101",
+				            text: "信用卡"
+				        }, {
+				            value: "120102",
+				            text: "蚂蚁花呗"
+				        }, {
+				            value: "120104",
+				            text: "京东白条"
+				        }
+				        ]
+				    },{
+				    	value:'130000',
+				    	text:'金融账户',
+				    	children:[{
+				    		value:"130101",
+				    		text:"投资账户"
+				    	},
+				    	{
+				    		value:"130102",
+				    		text:"XX银行"
+				    	},
+				    	{
+				    		value:"130103",
+				    		text:"黄金钱包"
+				    	}
+				    	]
+				    },{
+				    	value:'140000',
+				    	text:'虚拟账户',
+				    	children:[{
+				    		value:"140101",
+				    		text:"微信钱包"
+				    	},
+				    	{
+				    		value:"140102",
+				    		text:"支付宝"
+				    	},
+				    	{
+				    		value:"140103",
+				    		text:"首付"
+				    	},
+				    	{
+				    		value:"140104",
+				    		text:"商贷本金"
+				    	},
+				    	{
+				    		value:"140105",
+				    		text:"商贷利息"
+				    	},
+				    	{
+				    		value:"140106",
+				    		text:"公积金本金"
+				    	},
+				    	{
+				    		value:"140107",
+				    		text:"公积金利息"
+				    	},
+				    	{
+				    		value:"140108",
+				    		text:"公司工卡"
+				    	}
+				    	]
+				    },{
+				    	value:'150000',
+				    	text:'投资账户',
+				    	children:[{
+				    		value:"150101",
+				    		text:"基金账户"
+				    	},
+				    	{
+				    		value:"150102",
+				    		text:"余额宝"
+				    	},
+				    	{
+				    		value:"150103",
+				    		text:"股票账户"
+				    	}
+				    	]
+				    },{
+				    	value:'160000',
+				    	text:'负债账户',
+				    	children:[{
+				    		value:"160101",
+				    		text:"应付款项"
+				    	}
+				    	]
+				    },{
+				    	value:'170000',
+				    	text:'学习进修',
+				    	children:[{
+				    		value:"170101",
+				    		text:"书报杂志"
+				    	},
+				    	{
+				    		value:"170102",
+				    		text:"培训进修"
+				    	},
+				    	{
+				    		value:"170103",
+				    		text:"数码装备"
+				    	}
+				    	]
+				    },{
+				    	value:'180000',
+				    	text:'债券账户',
+				    	children:[{
+				    		value:"180101",
+				    		text:"应收款项"
+				    	}
+				    	]
+				    }
+				    ]
